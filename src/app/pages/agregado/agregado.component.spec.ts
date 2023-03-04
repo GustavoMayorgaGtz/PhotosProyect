4af9e6bcff2fe4e1d23288065f250c9e49fcf358
Gustavo Mayorga <redvioletoficial@gmail.com>
2023-03-04 09:38:35 -0600

    save

---
+++ src/app/pages/agregado/agregado.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AgregadoComponent } from './agregado.component';
+
+describe('AgregadoComponent', () => {
+  let component: AgregadoComponent;
+  let fixture: ComponentFixture<AgregadoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AgregadoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AgregadoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/agregado/agregado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregado',
+  templateUrl: './agregado.component.html',
+  styleUrls: ['./agregado.component.scss']
+})
+export class AgregadoComponent {
+
+}
